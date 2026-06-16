79cd40196886accd11cc8c656b535a8ba9315e6b
unconnected <myvie011@gmail.com>
2026-06-16 11:33:17 +0300

    i fucked up

--- LuaValue.cs
+++ LuaValue.cs
@@ -2,6 +2,24 @@ namespace Arbiter;
 
 public sealed class LuaValue
 {
+    public override string ToString()
+    {
+        switch (Kind)
+        {
+            case ValueKind.String:
+                return StringValue ?? "";
+
+            case ValueKind.Number:
+                return NumberValue?.ToString() ?? "0";
+
+            case ValueKind.Boolean:
+                return BooleanValue?.ToString().ToLowerInvariant() ?? "false";
+
+            default:
+                return "nil";
+        }
+    }
+
     public enum ValueKind
     {
         String,
@@ -21,5 +39,5 @@ public sealed class LuaValue
     }
     public static LuaValue FromString(string value) => new(ValueKind.String, value, null, null);
     public static LuaValue FromNumber(double value) => new(ValueKind.Number, null, value, null);
-    public static LuaValue FromBool(bool value) => new(ValueKind.Boolean, null, null, value);
+    public static LuaValue FromBool(bool value) => new(ValueKind.Boolean, null, null, value);   
 }
